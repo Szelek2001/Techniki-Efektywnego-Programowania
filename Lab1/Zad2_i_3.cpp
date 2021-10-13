a8ed6951c5e1c803cc94bfe6498987c3f068ecc1
Rafał <rafalkruszyna10@gmail.com>
2021-10-13 19:58:47 +0200

    Add 3zad

--- Lab1/Zad2_i_3.cpp
+++ Lab1/Zad2_i_3.cpp
@@ -9,17 +9,32 @@ bool b_alloc_table_2_dim(int ***piTable, int iSizeX, int iSizeY) {
     *piTable = new int*[iSizeX];
 
     for (int i = 0; i < iSizeX; ++i) {
-        (*piTable)[i] = new int[iSizeY];
+        *(*piTable+i) = new int[iSizeY];
 
     }
 
     return true;
 
 }
+
+bool b_dealloc_table_2_dim(int **piTable, int iSizeX, int iSizeY) {
+
+    if(iSizeX<=0||iSizeY<=0)
+        return false;
+
+
+    for (int i = 0; i < iSizeX; i++) {
+        delete[] *(piTable + i);
+    }
+    delete[] piTable;
+    return true;
+
+}
+
 int main() {
 
     int **pi_table;
     b_alloc_table_2_dim(&pi_table, 5, 3);
-
+    b_dealloc_table_2_dim(pi_table, 5, 3);
     return 0;
 }
