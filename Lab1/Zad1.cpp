23a4de86169db17906f1af1909c07b7e9c490b3f
Rafał <rafalkruszyna10@gmail.com>
2021-10-14 12:45:10 +0200

    Add .h and update CTable

--- Lab1/Zad1.cpp
+++ Lab1/Zad1.cpp
@@ -1,14 +1,20 @@
+//
+// Created by Rafał on 14.10.2021.
+//
+
+#include "Zad1.h"
 #include <iostream>
+
 using namespace std;
 void v_alloc_table_add_5(int table_size){
     if(table_size<=0){
-        cout<<"invalid number";
+        cout<<"invalid number \n";
         return;}
     int *table;
     table = new int[table_size];
 
     for (int i = 0; i < table_size; ++i) {
-        *(table+i) = i+5;
+        *(table+i) = i+DEFAULT_LENGTH;
     }
     for (int i = 0; i < table_size; ++i) {
         cout<<*(table+i)<<"\n";
---
+++ Lab1/Zad1.h
@@ -0,0 +1,7 @@
+#ifndef TEP_ZAD1_H
+#define TEP_ZAD1_H
+
+
+const int DEFAULT_LENGTH = 5;
+void v_alloc_table_add_5(int);
+#endif //TEP_ZAD1_H
