9914c0f42c97c0d775e847482abb1c8bc45b7a95
Rafał <rafalkruszyna10@gmail.com>
2021-11-01 22:08:57 +0100

    Add 2nd Lab

---
+++ Lab2/CTable.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include "CTable.h"
+
+
+using namespace std;
+
+const string CTable::DEFAULT_NAME = "default";
+
+CTable::CTable() {
+    s_name = DEFAULT_NAME;
+    i_table_length = DEFAULT_LENGTH;
+    pi_table = new int[i_table_length];
+    cout << "bezp: " << s_name << "\n";
+}
+
+CTable::CTable(string sName, int iTableLen) {
+    if(iTableLen<0)
+        iTableLen = DEFAULT_LENGTH;
+    s_name = sName;
+    i_table_length = iTableLen;
+    pi_table = new int[i_table_length];
+    cout << "parametr: " << s_name << endl;
+
+}
+
+CTable::CTable(const CTable &pcOther) {
+    i_table_length = pcOther.i_table_length;
+    pi_table = new int[i_table_length];
+    s_name = pcOther.s_name + "_copy";
+    cout << "kopiuj: " << s_name << endl;
+
+    for (int i = 0; i < i_table_length; ++i)
+        pi_table[i] = pcOther.getTable()[i];
+
+}
+
+CTable::~CTable() {
+    cout << "usuwam: " << s_name << "\n";
+    delete[] pi_table;
+}
+
+void CTable::setName(string sName) {
+    s_name = sName;
+}
+
+const string &CTable::getSName() const {
+    return s_name;
+}
+
+bool CTable::bSetNewSize(int iTableLen) {
+    if (iTableLen <= 0) return false;
+
+    if (i_table_length < iTableLen) {
+        int *tempTable = new int[iTableLen];
+        for (int i = 0; i < i_table_length; ++i) {
+            tempTable[i] = pi_table[i];
+        }
+        delete[] pi_table;
+        pi_table = tempTable;
+    }
+    i_table_length = iTableLen;
+
+    return true;
+}
+
+int CTable::getITableLength() const {
+    return i_table_length;
+}
+
+
+int *CTable::getTable() const {
+    return pi_table;
+}
+
+
+void vModTabPointer(CTable *cTab, int inewSize) {
+    cTab->bSetNewSize(inewSize);
+}
+
+void vModTabNoPointer(CTable cTab, int inewSize) {
+    cTab.bSetNewSize(inewSize);
+}
+
+void vStaticAllocation() {
+    CTable cTableStatic("romb", 10);
+    cTableStatic.bSetNewSize(20);
+
+}
+
+void vDynamicAllocation() {
+    CTable *cTableDynamic = new CTable();
+    delete cTableDynamic;
+}
+
+void CTable::operator=(CTable &pcOther)
+{
+    pi_table = pcOther.pi_table;
+    i_table_length = pcOther.i_table_length;
+}
+void CTable::vPrint()
+{
+    cout << "\n";
+    for (int i = 0; i < i_table_length; i++)
+    {
+        cout << pi_table[i];
+        cout << " ";
+    }
+}
+
+void CTable::vSetValueAt(int iOffset, int iNewVal)
+{
+    // Sprawdzenie prawidłowości parametru
+    if (iOffset < 0)
+    {
+        cout << "\nWartosc parametru iOffset nieprawidlowa.\n";
+    }
+    else
+    {
+        pi_table[iOffset] = iNewVal;
+    }
+}
+
+;;
+
+CTable CTable::operator+(CTable& other) {
+    CTable newTable(this->s_name + " + " + other.s_name, this->i_table_length + other.i_table_length);
+
+    for (int i = 0; i < this->i_table_length; i++) {
+        newTable.vSetValueAt(i, this->pi_table[i]);
+    }
+
+    for (int i = this->i_table_length; i < newTable.i_table_length; i++) {
+        newTable.vSetValueAt(i, other.pi_table[i - this->i_table_length]);
+    }
+
+    return newTable;
+}
+
+
+int main(){
+
+    CTable c_tab_0, c_tab_1;
+    c_tab_0.bSetNewSize(1);
+    c_tab_1.bSetNewSize(4);
+
+    c_tab_0.vSetValueAt(0, 11);
+    c_tab_1.vSetValueAt(0, 12);
+    c_tab_1.vSetValueAt(1, 13);
+    c_tab_1.vSetValueAt(2, 14);
+    c_tab_1.vSetValueAt(3, 15);
+
+
+    (c_tab_0+c_tab_1).vPrint();
+
+
+}
---
+++ Lab2/CTable.h
@@ -0,0 +1,61 @@
+#ifndef TEP_CTABLE_H
+#define TEP_CTABLE_H
+
+#include <string>
+
+using namespace std;
+
+
+class CTable {
+public:
+    void operator=(CTable &pcOther);
+    CTable operator+(CTable &pcOther);
+
+    CTable();
+
+    CTable(string s_name, int i_table_length);
+
+    CTable(const CTable &pcOthers);
+
+    ~CTable();
+
+    static const string DEFAULT_NAME;
+    static const int DEFAULT_LENGTH = 5;
+
+    bool bSetNewSize(int iTableLen);
+
+
+    CTable *pcClone() {
+        CTable *newCTable;
+        newCTable = new CTable(*this);
+
+        return newCTable;
+    }
+
+    int *getTable() const;
+
+    void setName(string sName);
+
+    const string &getSName() const;
+
+    int getITableLength() const;
+
+    void vPrint();
+    void vSetValueAt(int iOffset, int iNewVal);
+private:
+    string s_name;
+    int i_table_length;
+    int *pi_table;
+
+
+};
+
+void vModTabNoPointer(CTable cTab, int inewSize);
+
+void vModTabPointer(CTable *cTab, int);
+
+void vStaticAllocation();
+
+void vDynamicAllocation();
+
+#endif //TEP_CTABLE_H
