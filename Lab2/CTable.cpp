3adfc3242aee313ce1647aa7f4c5b9d0ebc7f3f6
Rafał <rafalkruszyna10@gmail.com>
2021-11-03 22:13:13 +0100

    add test

--- Lab2/CTable.cpp
+++ Lab2/CTable.cpp
@@ -92,11 +92,6 @@ void vDynamicAllocation() {
     delete cTableDynamic;
 }
 
-void CTable::operator=(CTable &pcOther) {
-    pi_table = pcOther.pi_table;
-    i_table_length = pcOther.i_table_length;
-}
-
 void CTable::vPrint() {
     cout << "\n";
     for (int i = 0; i < i_table_length; i++) {
@@ -128,21 +123,45 @@ CTable CTable::operator+(CTable &other) {
     return newTable;
 }
 
+//void CTable::operator=(CTable &pcOther) {
+//    bSetNewSize(pcOther.getITableLength());
+//    for (int i = 0; i < i_table_length; ++i) {
+//        pi_table[i] = pcOther.getTable()[i];
+//    }
+//}
+
+void CTable::operator=(CTable &pcOther) {
+    pi_table = pcOther.pi_table;
+    i_table_length = pcOther.i_table_length;
+}
 
 int main() {
 
+//    CTable c_tab_0, c_tab_1;
+//    c_tab_0.bSetNewSize(1);
+//    c_tab_1.bSetNewSize(4);
+//
+//    c_tab_0 = c_tab_1;
+
+
     CTable c_tab_0, c_tab_1;
-    c_tab_0.bSetNewSize(1);
+    c_tab_0.bSetNewSize(6);
     c_tab_1.bSetNewSize(4);
+/* initialize table */
+    c_tab_0 = c_tab_1;
+    c_tab_1.vSetValueAt(2, 123);
+    c_tab_0.vPrint();
+    c_tab_1.vPrint();
 
-    c_tab_0.vSetValueAt(0, 11);
-    c_tab_1.vSetValueAt(0, 12);
-    c_tab_1.vSetValueAt(1, 13);
-    c_tab_1.vSetValueAt(2, 14);
-    c_tab_1.vSetValueAt(3, 15);
 
-
-    (c_tab_0 + c_tab_1).vPrint();
+//    c_tab_0.vSetValueAt(0, 11);
+//    c_tab_1.vSetValueAt(0, 12);
+//    c_tab_1.vSetValueAt(1, 13);
+//    c_tab_1.vSetValueAt(2, 14);
+//    c_tab_1.vSetValueAt(3, 15);
+//
+//
+//    (c_tab_0 + c_tab_1).vPrint();
 
 
 }
